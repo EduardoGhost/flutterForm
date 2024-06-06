97a5a34a57927fc493af98420a0c037c3587d329
Eduardo Freire <61304055+EduardoGhost@users.noreply.github.com>
2024-06-06 13:47:25 -0300

    FEAT: add LoginScreen.dart

--- lib/routers/app_routers.dart
+++ lib/routers/app_routers.dart
@@ -2,5 +2,6 @@ class AppRouters{
   static const HOME = '/';
   static const USER_FORM = '/user-form';
   static const USER_DETAILS = '/user-details';
+  static const USER_LOGIN = '/user-login';
 
 }
